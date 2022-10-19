9e8cd161dbc6c4e72ccd23f24acbadda7b940d01
Brian Noh <noh24@ymail.com>
2022-10-18 22:20:43 -0400

    Add light+dark mode buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,32 @@
+// window.onload = function() {
+//   let h1 = document.querySelector("h1");
+//   h1.onmouseover = function() {
+//     alert("I'm a heading");
+//   };
+
+// let p = document.querySelector("p");
+//   p.onmouseover = function() {
+//     document.querySelector("p>em").innerText = "I've changed!";
+//   };
+
+// let img = document.querySelector("img");
+//   img.onmouseover = function() {
+//     img.style.width = "700px";
+//   };
+// }
+
 window.onload = function() {
-  let h1 = document.querySelector("h1");
-  h1.onmouseover = function() {
-    alert("I'm a heading");
-  };
+  let body = document.querySelector("body");
 
-let p = document.querySelector("p");
-  p.onmouseover = function() {
-    document.querySelector("p>em").innerText = "I've changed!";
-  };
+  let lightButton = document.querySelector(".light-mode");
+  lightButton.onclick = function() {
+    body.style.backgroundColor = "white";
+    body.style.color = "black";
+  }
 
-let img = document.querySelector("img");
-  img.onmouseover = function() {
-    img.style.width = "700px";
-  };
+  let darkButton = document.querySelector(".dark-mode");
+  darkButton.onclick = function() {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+  }
 }
-
